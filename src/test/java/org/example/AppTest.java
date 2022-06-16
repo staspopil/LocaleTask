a0fe6bb08413b29f643a64df8092b8882a45efd6
Stanislav Popil <s.popil@icg-engineering.com>
2022-06-16 14:27:25 +0300

    Temp 2 commit

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,6 +3,7 @@ package org.example;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -23,6 +24,14 @@ public class AppTest
     }
 
     @Test
-    public void testFormatBigDecimalBaseOnUSLocaleThatReturnsString(){
+    public void testFormatBigDecimalBaseOnEnUSLocaleThatReturnsString(){
+        String expectedString = "123,124.123";
+        Assert.assertEquals(expectedString, App.formatString(new Locale("en", "US"), new BigDecimal("123124.123123")));;
+    }
+
+    @Test
+    public void test2FormatBigDecimalBaseOnEnUSLocaleThatReturnsString(){
+        String expectedString = "1,111,111,111,111,111,111,111,111,111,111,111,111,111,111,111,111,111,111,111,111,111,111,123,124.111";
+        Assert.assertEquals(expectedString, App.formatString(new Locale("en", "US"), new BigDecimal("1111111111111111111111111111111111111111111111111111111111111111111123124.11111111111111111111111111111111111111111111111111111111111111231123")));;
     }
 }
